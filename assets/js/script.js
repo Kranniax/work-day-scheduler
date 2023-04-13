488b518c1c5634ea4ae46621a53cc68a121388da
Shane Wade <shanebramble96@hotmail.com>
2023-04-13 18:51:52 -0500

    block is color-coded to indicate whether it is in the past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 var currentDay = moment();
 var currentHour = currentDay.hour();
 var currenDayEl = $("#currentDay");
-
+console.log(currentHour);
 // display current day and time.
 currenDayEl.text(currentDay.format("dddd, MMMM Do YYYY"));
 
+// check each time block against the current time.
+var timeBlockUpdate = function () {
+  $(".time-block").each(function () {
+    var timeBlock = parseInt($(this).attr("id").replace("hour-", ""));
 
+    if (timeBlock < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlock === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+};
 
-
+timeBlockUpdate();
